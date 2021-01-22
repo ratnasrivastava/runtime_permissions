42bef3f0f59f8b29baa5feadad42ad34be1e8cbd
Ratna Srivastava <ratna.srivastava@letstransport.team>
2021-01-22 15:02:21 +0530

    ADDED :: Location permission added

--- app/src/main/java/com/example/runtimepermission/MainActivity.java
+++ app/src/main/java/com/example/runtimepermission/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -14,16 +15,20 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int STORAGE_PERMISSION_CODE = 100;
+    private static final int GPS_PERMISSION_CODE = 101;
+    private static final int GPS_BACKGROUND_PERMISSION_CODE = 102;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, GPS_PERMISSION_CODE);
     }
 
-    private void checkPermission(String writeExternalStorage, int storagePermissionCode) {
-        if(ContextCompat.checkSelfPermission(MainActivity.this, writeExternalStorage) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[]{writeExternalStorage},storagePermissionCode);
+    private void checkPermission(String permission, int permissionCode) {
+        if(ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{permission},permissionCode);
         }
         else{
             Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
@@ -39,6 +44,14 @@ checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_C
             }
             else{
                 Toast.makeText(MainActivity.this, "Storage permission is denied by User", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if(requestCode == GPS_PERMISSION_CODE){
+            if(grantedResults.length > 0 && grantedResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this, "GPS permission is already granted", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "GPS permission is denied by User", Toast.LENGTH_SHORT).show();
             }
         }
     }
